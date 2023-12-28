0dc5e345dcae37e4b0cf83fb81f05d317e73eadb
ryiann <me@ryana.cn>
2023-12-28 23:05:30 +0800

    update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -200,6 +200,13 @@ export function SideBar(props: { className?: string }) {
       </div>
 
       <div className={styles["sidebar-tail"]}>
+        <div className={styles["sidebar-actions"]}>
+          <div className={styles["sidebar-action"]}>
+            <a href={REPO_URL} target="_blank" rel="noopener noreferrer">
+              <IconButton icon={<GithubIcon />} shadow />
+            </a>
+          </div>
+        </div>
         <div>
           <IconButton
             icon={<AddIcon />}
